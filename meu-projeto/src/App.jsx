131c108904b87351181cd2dd20847d5641e30184
Tiago Efrens <98785464+tiagoefrens@users.noreply.github.com>
2024-07-03 19:41:21 -0300

    props

--- meu-projeto/src/App.jsx
+++ meu-projeto/src/App.jsx
@@ -1,13 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-  return (
+
+  const nome = 'Maria'
+  
+   return (
+    
     <div className="App">
-      <h1>Olá React</h1>
-      <p>Meu primeiro App</p>
+    
+      <HelloWorld/>
+      <SayMyName nome = "Tiago"/>
+      <SayMyName nome = "João"/>
+      <SayMyName nome = {nome}/>
+      <Pessoa
+        nome = "Tiago"
+        idade = "28"
+        profissao = "Programador"
+        foto = "https://via.placeholder.com/150"
+      />
+
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ meu-projeto/src/components/Frase.js
@@ -0,0 +1,12 @@
+function Frase(){
+
+    return(
+        <div>
+            <p>Este é um componente  com uma frase</p>
+        </div>
+    )
+
+}
+
+
+export default Frase
