6b7234b86edc2ce645dad7668b88bf33e5ea7965
Tiago Efrens <98785464+tiagoefrens@users.noreply.github.com>
2024-07-10 20:54:50 -0300

    renderização condicional

--- meu-projeto/src/App.jsx
+++ meu-projeto/src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css';
-import Evento from './components/Evento';
-import Form from './components/Form';
+
+import Condicional from './components/Condicional';
 
 
 function App() {
@@ -11,11 +11,10 @@ function App() {
    return (
     
     <div className="App">
-      <h1>Testando Eventos</h1>
-      <Evento numero = "1"/>
-      <Evento numero = "2"/>
+      <h1>Renderização Condicional</h1>
 
-      <Form/>
+      <Condicional/>
+      
 
   
 
