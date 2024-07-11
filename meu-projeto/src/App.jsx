a19773d2f42713b9c81f471358fb09ec16a24ba8
Tiago Efrens <98785464+tiagoefrens@users.noreply.github.com>
2024-07-11 20:41:39 -0300

    renderização

--- meu-projeto/src/App.jsx
+++ meu-projeto/src/App.jsx
@@ -1,22 +1,21 @@
 
 import './App.css';
-
+import OutraLista from './components/OutraLista';
 import Condicional from './components/Condicional';
 
 
 function App() {
 
-  const nome = 'Maria'
+  const meusItens  = ['React', 'Vue', 'Angular']
   
    return (
     
     <div className="App">
-      <h1>Renderização Condicional</h1>
+      <h1>Renderização de Listas</h1>
+      <OutraLista itens={meusItens}/>
+      <OutraLista itens={[]}/>
 
-      <Condicional/>
       
-
-  
 
 
     </div>
